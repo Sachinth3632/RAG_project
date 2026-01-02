f2826d0b600cf9fccc11caf1bc8d006caab7e928
Sachinth3632 <sachin.t.hemareddy@gmail.com>
2026-01-02 12:39:59 +0530

    Create ingest.py

---
+++ ingest.py
@@ -0,0 +1,14 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+loader = PyPDFLoader("data/sample.pdf")
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+chunks = splitter.split_documents(docs)
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(chunks, embeddings)
+db.save_local("vectorstore")
